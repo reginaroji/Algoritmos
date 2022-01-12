f30cf2ce58565f344b8096a510706fe94bb385ca
Regina Roji <reginaroji@gmail.com>
2022-01-11 18:24:24 -0600

    ejercicio tipos de datos y variables

---
+++ Tipos de datos y variables/converter.js
@@ -0,0 +1,96 @@
+// Part 1: Type Converter
+
+// console.log(false);
+console.log(Number(false));
+console.log(String(false));
+console.log(Boolean(false));
+
+// console.log(true);
+console.log("\n");
+console.log(Number(true));
+console.log(String(true));
+console.log(Boolean(true));
+
+// console.log(0);
+console.log("\n");
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+
+// console.log(1);
+console.log("\n");
+console.log(Number(1));
+console.log(String(1));
+console.log(Boolean(1));
+
+//console.log("0");
+console.log("\n");
+console.log(Number("0"));
+console.log(String("0"));
+console.log(Boolean("0"));
+
+//console.log("000");
+console.log("\n");
+console.log(Number("000"));
+console.log(String("000"));
+console.log(Boolean("000"));
+
+console.log("\n");
+//console.log("1");
+console.log(Number("1"));
+console.log(String("1"));
+console.log(Boolean("1"));
+
+console.log("\n");
+//console.log(NaN);
+console.log(Number(NaN));
+console.log(String(NaN));
+console.log(Boolean(NaN));
+
+console.log("\n");
+//console.log(Infinity);
+console.log(Number(Infinity));
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
+
+console.log("\n");
+//console.log(-Infinity);
+console.log(Number(-Infinity));
+console.log(String(-Infinity));
+console.log(Boolean(-Infinity));
+
+console.log("\n");
+//console.log("");
+console.log(Number(""));
+console.log(String(""));
+console.log(Boolean(""));
+
+console.log("\n");
+//console.log("20");
+console.log(Number("20"));
+console.log(String("20"));
+console.log(Boolean("20"));
+
+console.log("\n");
+//console.log("Twenty");
+console.log(Number("Twenty"));
+console.log(String("Twenty"));
+console.log(Boolean("Twenty"));
+
+console.log("\n");
+//console.log(null);
+console.log(Number(null));
+console.log(String(null));
+console.log(Boolean(null));
+
+console.log("\n");
+//console.log(undefined);
+console.log(Number(undefined));
+console.log(String(undefined));
+console.log(Boolean(undefined));
+
+//console.log("\n");
+// console.log(Number(undefinited));
+// console.log(String(undefinited));
+// console.log(Boolean(undefinited));
+console.log("\n");
---
+++ Tipos de datos y variables/new-contender.js
@@ -0,0 +1,12 @@
+var añoNacimiento = 1997;
+var añoFuturo = 2050;
+var edad = añoFuturo - añoNacimiento;
+
+console.log("Si naciste en " + añoNacimiento + ", en " + añoFuturo + " tendrás " + (edad - 1) + " o " + edad + " años dependiendo del mes.");
+
+
+var nombre = "Juanito";
+var ubicacion = "Perú";
+var hobby = "tejer";
+
+console.log("Hola, mi nombre es " + nombre + ". Vivo en " + ubicacion + " y me gusta " + hobby + ".")
