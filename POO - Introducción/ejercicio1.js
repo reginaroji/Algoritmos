2e1106512ce3c80cff8ba12230cd385c16c53f25
Regina Roji <reginaroji@gmail.com>
2022-01-20 22:04:47 -0600

    Prácticas de POO

---
+++ POO - Introducción/ejercicio1.js
@@ -0,0 +1,23 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Describe() {
+        return (this.name + ", " + this.age + " years old");
+    }
+}
+
+let myData = new Person("Sofia", 23);
+console.log(myData.Describe());
+
+/*Part 2 HTML
+
+Create an HTML page that has 2 input boxes: one for name and another for age. When someone enters a name and age, the page will show a list of "{Name}, {x} years old". */
+
+var nombre = prompt("Ingresa tu nombre: ");
+var edad = prompt("Ingresa tu edad: ");
+
+let ourData = new Person(nombre,edad);
+
+document.getElementById("person").innerHTML = ourData.Describe();
